2d2d546f265264779278d8bb7cffec9a8d9ce489
mohitkumartoshniwal <mohitkrtoshniwal.com>
2020-04-24 00:29:25 +0530

    connected create-exercise and create-user component to backend endpoints

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class CreateUser extends Component {
 
@@ -22,6 +23,8 @@ export default class CreateUser extends Component {
           username: this.state.username,
         };
         console.log(newUser);
+        axios.post('http://localhost:5000/users/add', newUser)
+  .then(res => console.log(res.data));
         
         this.setState({
           username: ''
